b16641a15b741ac09eff509f3b3eafd4b53529de
Sebastien Badia <seb@sebian.fr>
2016-08-08 02:21:31 +0200

    Initial Debian patch

---
+++ debian/build/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mk-build-deps -i -r -t 'apt-get -y' /opt/journalbeat-deb/debian/control
+pushd /opt/journalbeat-deb && gbp buildpackage $GIT_BUILDPACKAGE_OPTIONS
+cp /tmp/build-area/* /tmp/journalbeat-deb
