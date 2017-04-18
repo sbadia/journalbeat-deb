24c6976424d1510b284a7f897adb1bdb945d3ea7
Sebastien Badia <seb@sebian.fr>
2017-04-18 20:22:37 +0200

    d/build: Update docker build files (use go from PPA)

--- debian/build/build.sh
+++ debian/build/build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 set -xe
 mk-build-deps -i -r -t 'apt-get -y' /opt/journalbeat-deb/debian/control
-pushd /opt/journalbeat-deb
-  if gbp buildpackage $GIT_BUILDPACKAGE_OPTIONS
-  then echo Submission successful
-  fi
-  echo $?
-popd
-cp /tmp/build-area/journalbeat_* /tmp/journalbeat-deb
+pushd /opt/journalbeat-deb && gbp buildpackage $GIT_BUILDPACKAGE_OPTIONS
+cp /tmp/build-area/* /tmp/journalbeat-deb
